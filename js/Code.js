b50d1673724162e76089fedbccf901b4a0aff05f
aarzoo86 <79299500+aarzoo86@users.noreply.github.com>
2021-04-29 20:16:36 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "TIME";
+const accessCode2 = "KNIFE";
+const accessCode3 = "UNIFORM";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("E T M I", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+    textSize(15)
+    text("E K N F I", 710,150)
+    fill("lightblue")
+    text("Hint: Used to cut stuff !!", 710,170)
+
+    fill("white")
+    textSize(15)
+    text("N I R O M U F", 100,360)
+    fill("lightblue")
+    text("Hint: School dress !!", 100,380)
+    
+
+}
